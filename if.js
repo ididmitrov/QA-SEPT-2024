119885492cf894fe3755c5111e4d43e75cbfcf28
Philip Mihaylov <philipmihaylov93@gmail.com>
2024-10-15 23:59:38 +0300

    we are learning

---
+++ if.js
@@ -0,0 +1,11 @@
+function solve() {
+    let a = input[0]
+    if(a == "gitHub") {
+        console.log("Congrats you have made your pull request");
+    }else{
+        console.log("You have learn more");
+    }
+
+}
+input = ["gitHub"];
+solve()
