e71c3252c2b212f9257c2fc6c40570cedbb4f6ad
Potrebitel12 <179023649+Potrebitel12@users.noreply.github.com>
2024-10-16 00:07:38 +0300

    Changed some code in if.js

--- if.js
+++ if.js
@@ -1,11 +1,11 @@
-function solve() {
-    let a = input[0]
-    if(a == "gitHub") {
+function solve(some_input) {
+    if(some_input == "gitHub") {
         console.log("Congrats you have made your pull request");
-    }else{
+    } else {
         console.log("You have learn more");
     }
-
 }
-input = ["gitHub"];
-solve()
+input = "gitHub";
+solve(input);
+
+// changed some code
